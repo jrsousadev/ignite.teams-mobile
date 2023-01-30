d6142c3d3d22543038be8add253ef5d0bd2cc8c7
Junior Sousa <jrsousadev@gmail.com>
2023-01-30 14:32:51 -0300

    finishing project

---
+++ App.tsx
@@ -0,0 +1,26 @@
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+import { ThemeProvider } from "styled-components";
+import { Loading } from "@components/Loading";
+import { StatusBar } from "react-native";
+import { Routes } from "@routes/index";
+
+import theme from "./src/theme";
+
+export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  return (
+    <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <Routes /> : <Loading />}
+    </ThemeProvider>
+  );
+}
